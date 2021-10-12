1aa585fd39ae63c5f867ae8207bf894114918264
Amal Gafsi <amal.gafsi1@esprit.tn>
2021-10-12 15:48:05 +0100

    first

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Address from "./Components/Profile/Address";
+import FullName from "./Components/Profile/FullName";
+import ProfilePhoto from "./Components/Profile/ProfilePhoto";
+
+function App() {
+  return (
+    <div className="App">
+      <ProfilePhoto />
+      <FullName />
+      <Address />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Profile/ProfilePhoto.js
@@ -0,0 +1,10 @@
+import React from "react";
+import logo from "../Images/logo.png";
+
+export default function ProfilePhoto() {
+  return (
+    <div>
+      <img src={logo} alt="logo" />
+    </div>
+  );
+}
